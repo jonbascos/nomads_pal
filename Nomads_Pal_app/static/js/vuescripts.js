aad533341b9654b2a3840fea8e4a6e58cbc6f3b4
jonbascos <jon@jonbascos.com>
2019-10-07 14:56:32 -0700

    did a little styling with colors

--- Nomads_Pal_app/static/js/vuescripts.js
+++ Nomads_Pal_app/static/js/vuescripts.js
@@ -36,52 +36,33 @@ new Vue({
                 this.locations = response.data.results
             })
         },
-        uploadImage (event) {
-            console.log(event);
-            this.newLocation.locationPhoto = event.target.files[0];
-            console.log(this.newLocation.locationPhoto);
-        },
-        addLocation () {
+        
+        addLocation() {
             let csrf_token = document.querySelector("input[name=csrfmiddlewaretoken]").value;
-            let rawData = {
-                businessName: this.newLocation.businessName,
-                locationAddress: this.newLocation.locationAddress,
-                locationCity: this.newLocation.locationCity,
-                locationState: this.newLocation.locationState,
-                locationZipCode: this.newLocation.locationZipCode,
-                phoneNumber: this.newLocation.phoneNumber,
-                websiteUrl: this.newLocation.websiteUrl,
-                storeHours: this.newLocation.storeHours,
-                uploadSpeed: this.newLocation.uploadSpeed,
-                downloadSpeed: this.newLocation.downloadSpeed,
-            }
-            rawData = JSON.stringify(rawData)
             
-            const fd = new FormData();
-                fd.append('locationPhoto', this.newLocation.locationPhoto, this.newLocation.locationPhoto.name)
-                fd.append('data', rawData)
-                console.log(fd);
-            // axios({
-            //     method: 'post',
-            //     url: 'api/location/',
-            //     headers: {
-            //         "X-CSRFToken": csrf_token,
-            //         "Content-Type": "multipart/form-data",
-            //     },
-            //     data: { 
-            //         fd
-            //      },
-            //     }).then(response => {
-            //         console.log(response);
-            //         alert('Success');
-            //         this.addNewLocation = false;
-            //     })
-                axios.post('api/location/', fd, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                        "X-CSRFToken": csrf_token,
-                    }
-                });
+            axios({
+                method: 'post',
+                url: 'api/location/',
+                headers: {
+                    "X-CSRFToken": csrf_token,
+                },
+                data: { 
+                    businessName: this.newLocation.businessName,
+                    locationAddress: this.newLocation.locationAddress,
+                    locationCity: this.newLocation.locationCity,
+                    locationState: this.newLocation.locationState,
+                    locationZipCode: this.newLocation.locationZipCode,
+                    phoneNumber: this.newLocation.phoneNumber,
+                    websiteUrl: this.newLocation.websiteUrl,
+                    storeHours: this.newLocation.storeHours,
+                    uploadSpeed: this.newLocation.uploadSpeed,
+                    downloadSpeed: this.newLocation.downloadSpeed,
+                 },
+                }).then(response => {
+                    console.log(response);
+                    alert('Success');
+                    this.addNewLocation = false;
+                })
         },
         viewAddLocation: function () {
             if (this.clickedSubmit === true) {
@@ -124,47 +105,47 @@ new Vue({
 
 
 // businessName: this.newLocation.businessName,
-                    // locationPhoto: this.newLocation.locationPhoto,
-                    // locationAddress: this.newLocation.locationAddress,
-                    // locationCity: this.newLocation.locationCity,
-                    // locationState: this.newLocation.locationState,
-                    // locationZipCode: this.newLocation.locationZipCode,
-                    // phoneNumber: this.newLocation.phoneNumber,
-                    // websiteUrl: this.newLocation.websiteUrl,
-                    // storeHours: this.newLocation.storeHours,
-                    // uploadSpeed: this.newLocation.uploadSpeed,
-                    // downloadSpeed: this.newLocation.downloadSpeed,
-                // },
+// locationPhoto: this.newLocation.locationPhoto,
+// locationAddress: this.newLocation.locationAddress,
+// locationCity: this.newLocation.locationCity,
+// locationState: this.newLocation.locationState,
+// locationZipCode: this.newLocation.locationZipCode,
+// phoneNumber: this.newLocation.phoneNumber,
+// websiteUrl: this.newLocation.websiteUrl,
+// storeHours: this.newLocation.storeHours,
+// uploadSpeed: this.newLocation.uploadSpeed,
+// downloadSpeed: this.newLocation.downloadSpeed,
+// },
 
 
 
-                // addLocation () {
-                //     let csrf_token = document.querySelector("input[name=csrfmiddlewaretoken]").value;
-                //     const formData = new FormData();
-                //     formData.append('locationPhoto', this.newLocation.locationPhoto);
-                //     formData.append('businessName', this.newLocation.businessName);
-                //     formData.append('locationAddress', this.newLocation.locationAddress);
-                //     formData.append('locationCity', this.newLocation.locationCity);
-                //     formData.append('locationState', this.newLocation.locationState);
-                //     formData.append('locationZipCode', this.newLocation.locationZipCode);
-                //     formData.append('phoneNumber', this.newLocation.phoneNumber);
-                //     formData.append('websiteUrl', this.newLocation.websiteUrl);
-                //     formData.append('storeHours', this.newLocation.storeHours);
-                //     formData.append('uploadSpeed', this.newLocation.uploadSpeed);
-                //     formData.append('downloadSpeed', this.newLocation.downloadSpeed);
-        
-                //     axios({
-                //         method: 'post',
-                //         url: 'api/location/',
-                //         headers: {
-                //             "X-CSRFToken": csrf_token,
-                //             "Content-Type": "multipart/form-data",
-                //         },
-                //         data: { 
-                //             formData
-                //          },
-                //         }).then(response => {
-                //             console.log(response);
-                //             this.addNewLocation = false;
-                //         })
-                // },
+// addLocation () {
+//     let csrf_token = document.querySelector("input[name=csrfmiddlewaretoken]").value;
+//     const formData = new FormData();
+//     formData.append('locationPhoto', this.newLocation.locationPhoto);
+//     formData.append('businessName', this.newLocation.businessName);
+//     formData.append('locationAddress', this.newLocation.locationAddress);
+//     formData.append('locationCity', this.newLocation.locationCity);
+//     formData.append('locationState', this.newLocation.locationState);
+//     formData.append('locationZipCode', this.newLocation.locationZipCode);
+//     formData.append('phoneNumber', this.newLocation.phoneNumber);
+//     formData.append('websiteUrl', this.newLocation.websiteUrl);
+//     formData.append('storeHours', this.newLocation.storeHours);
+//     formData.append('uploadSpeed', this.newLocation.uploadSpeed);
+//     formData.append('downloadSpeed', this.newLocation.downloadSpeed);
+
+//     axios({
+//         method: 'post',
+//         url: 'api/location/',
+//         headers: {
+//             "X-CSRFToken": csrf_token,
+//             "Content-Type": "multipart/form-data",
+//         },
+//         data: { 
+//             formData
+//          },
+//         }).then(response => {
+//             console.log(response);
+//             this.addNewLocation = false;
+//         })
+// },
