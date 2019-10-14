512c1c46019c035ee71c829c46e641c247def7fd
jonbascos <jon@jonbascos.com>
2019-10-14 12:39:30 -0700

    Fixed CSS coding for results.  Now able to flex.  Working on refactoring addLocations to a Vue Component

--- Nomads_Pal_app/static/js/vuescripts.js
+++ Nomads_Pal_app/static/js/vuescripts.js
@@ -1,12 +1,56 @@
+
+// Vue.component('addnewlocation', {
+//     props: ['businessName']['newLocation'],
+//     template: `
+//         <form @submit.prevent='addLocation' id='addNewForm'>
+//             <h1>Add New Location</h1>
+//             {% csrf_token %}
+//             <label class='add_input' for='businessName'>Business Name:</label>
+//             <input v-model='newLocation.businessName' id='businessName' placeholder='Name of Business'>
+
+//             <label class='add_input' for='address'>Address:</label>
+//             <input v-model='newLocation.locationAddress' id='address' placeholder='Address'>
+
+//             <label class='add_input' for='city'>City:</label>
+//             <input id='city' placeholder='City' :value="newLocation.locationCity"
+//             @input="newLocation.locationCity = $event.target.value.toUpperCase()">
+
+//             <label class='add_input' for='state'>State:</label>
+//             <input id='state' placeholder='State: OR' :value="newLocation.locationState"
+//             @input="newLocation.locationState = $event.target.value.toUpperCase()">
+
+//             <label class='add_input' for='zipcode'>Zipcode/Postal Code:</label>
+//             <input v-model='newLocation.locationZipCode' id='zipcode' placeholder='Zipcode/Postal Code'>
+
+//             <label class='add_input' for='phoneNumber'>Phone Number:</label>
+//             <input v-model='newLocation.phoneNumber' id='phoneNumber' placeholder='Phone Number (optional)'>
+
+//             <label class='add_input' for='website'>Website:</label>
+//             <input v-model='newLocation.websiteUrl' id='website' placeholder='http://www.google.com (optional)'>
+
+//             <label class='add_input' for='storeHours'>Store hours:</label>
+//             <input v-model='newLocation.storeHours' id='storeHours' placeholder='Store Hours (optional)'>
+
+//             <label class='add_input' for='downloadSpeed'>Download Speed (in Mbps): </label>
+//             <input v-model='newLocation.downloadSpeed' id='downloadSpeed' placeholder='downloadSpeed'>
+
+//             <label class='add_input' for='uploadSpeed'>Upload Speed (in Mbps): </label>
+//             <input v-model='newLocation.uploadSpeed' id='uploadSpeed' placeholder='uploadSpeed'>
+
+//             <button class="btn waves-effect waves-light" type="submit" @click.prevent='addLocation'>Submit</button>
+//         </form>
+//     `
+// })
+
+
+
 Vue.component('results', {
     props: ['location'],
-
     template: `
         <div>
-            <div class="location-card-area">
+            <ul>
                 <div class="location-card">
-                    <ul >
-                        <li>{{ location.businessName }}</li>
+                        <li><a :href=location.websiteUrl>{{ location.businessName }}</a></li>
                         <li><img src="https://placeimg.com/200/200/tech"></li>
                         <li>Address: {{ location.locationAddress }}<br> {{ location.locationCity}}, {{ location.locationState}} {{ location.locationZipCode }}</li>
                         <button @click="detailstoggle = ! detailstoggle" class="btn">Details</button>
@@ -17,9 +61,8 @@ Vue.component('results', {
                             <li>Download Speed: {{ location.downloadSpeed }} Mbps</li>
                             <li>Upload Speed: {{ location.uploadSpeed }} Mbps</li>
                         </div>
-                    </ul>
                 </div>
-            </div>
+            </ul>
         </div>
     `,
 
